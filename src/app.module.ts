79ef2cb8e1a3dd6d79e3204cdb163c1bcbeb1769
Cesar Bacca <bacca2095@gmail.com>
2022-03-20 01:04:57 -0500

    feat: agregada conexion a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
